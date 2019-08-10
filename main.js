68345aad1532467b98df433ea84ab2901c065cf4
HappySeal <cafer15165@gmail.com>
2019-08-10 21:27:44 +0300

    Requesting scoreboard

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const cheerio = require('cheerio');
-
+let page = cheerio.load("http://gmscoreboard.com/handle_score.php?tagid=5d4f09c4e525b15654609322539&getscore=10");
+console.log(page);
 var city = [['Edirne',''],[ 'Kırklareli',''],[ 'Tekirdağ',''],[ 'İstanbul',''],[ 'Kocaeli',''],[ 'Yalova',''],[ 'Sakarya',''],[ 'Düzce',''],[ 'Zonguldak',''],[ 'Bartın',''],[ 'Kastamonu',''],[ 'Sinop',''],[ 'Samsun',''],[ 'Ordu',''],[ 'Giresun',''],[ 'Trabzon',''],[ 'Rize',''],[ 'Artvin',''],[ 'Ardahan',''],[ 'Erzurum',''],[ 'Bayburt',''],[ 'Gümüşhane',''],[ 'Sivas',''],[ 'Tokat',''],[ 'Amasya',''],[ 'Çorum',''],[ 'Çankırı',''],[ 'Bolu',''],[ 'Bilecik',''],[ 'Balıkesir',''],[ 'Çanakkale',''],[ 'İzmir',''],[ 'Manisa',''],[ 'Kütahya',''],[ 'Eskişehir',''],[ 'Ankara',''],[ 'Kırıkkale',''],[ 'Yozgat',''],[ 'Erzincan',''],[ 'Kars',''],[ 'Ağrı',''],[ 'Iğdır',''],[ 'Van',''],[ 'Hakkari',''],[ 'Şirnak',''],[ 'Siirt',''],[ 'Bitlis',''],[ 'Muş',''],[ 'Bingöl',''],[ 'Diyarbakır',''],[ 'Mardin',''],[ 'Şanlıurfa',''],[ 'Adıyaman',''],[ 'Elazığ',''],[ 'Tunceli',''],[ 'Malatya',''],[ 'Kahramanmaraş',''],[ 'Gaziantep',''],[ 'Batman',''],[ 'Kayseri',''],[ 'Adana',''],[ 'Osmaniye',''],[ 'Hatay',''],[ 'Nevşehir',''],[ 'Niğde',''],[ 'Mersin',''],[ 'Karaman',''],[ 'Konya',''],[ 'Aksaray',''],[ 'Kilis',''],[ 'Antalya',''],[ 'Isparta',''],[ 'Afyonkarahisar',''],[ 'Uşak',''],[ 'Denizli',''],[ 'Muğla',''],[ 'Aydin',''],[ 'Bursa',''],[ 'Kırşehir',''],[ 'Karabük',''],[ 'Burdur','']];
 
 var score = [0,0];
